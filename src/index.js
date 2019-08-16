21cd2c1c0805548c8cf118c5a6c7367a96b074a7
Jill Fronk <jillichrome@Jills-MacBook-Pro.local>
2019-08-16 17:21:37 -0400

    Background update & update gitignore

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './app';
-import { createStore } from 'redux';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './store/reducers/rootReducer';
 
