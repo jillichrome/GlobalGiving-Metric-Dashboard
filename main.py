0f03b4c87fc32558a6fda8a6f1e59338b797eaea
Joe Gunyon <7231693+sgunyon@users.noreply.github.com>
2019-08-17 16:49:17 -0400

    Added json data

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 # importing the requests library
+import json
 import requests
 import config as cfg
+
 
 # api-endpoint
 URL = "https://api.globalgiving.org/api/public/projectservice/all/projects/active"
@@ -13,7 +15,8 @@ HEADERS = {'Content-type': 'application/json', 'Accept': 'application/json'}
 PARAMS = {'api_key':cfg.API_KEY, 'nextProjectId':354}
 
 # sending get request and saving the response as response object
-r = requests.get(url = URL, params = PARAMS, headers = HEADERS)
+RESPONSE = requests.get(url=URL, params=PARAMS, headers=HEADERS)
 
 # extracting data in json format
-print(r.json())
+INFO = RESPONSE.json()
+JOBJ = json.dumps(INFO)
