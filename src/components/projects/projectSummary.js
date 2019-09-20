e7c0e1518dc7d340dd447cef04b1c56e4c6670c7
jillichrome <jmfronk.17@gmail.com>
2019-09-20 14:16:43 -0400

    Fix image sizing

--- src/components/projects/projectSummary.js
+++ src/components/projects/projectSummary.js
@@ -3,11 +3,11 @@ import React from 'react';
 const ProjectSummary = ({project}) => {
   return(
     <div className='card horizontal grey lighten-1 z-depth-0 project-summary'>
-      <div className='card-image responsive-img'>
-        <img src={project.image} alt='' />
+      <div className='card-image'>
+        <img src={project.image} alt='' className='responsive-img' />
       </div>
       <div className='card-content'>
-        <span className='center'><h5>{project.title}</h5></span>
+        <span><h6 className='center-align black-text darken-4'>{project.title}</h6></span>
         <p>{project.organization} | {project.theme}</p>
         <p>Goal: {project.goal}</p>
         <p>Current Status: {project.funding}</p>
